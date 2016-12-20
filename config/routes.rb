6e8bf958351c41766b55f08f756c356948cb2ad7
Garrett Smiley <garrettsmiley@msn.com>
2016-12-19 17:04:13 -0700

    created sign in functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users
+  as :user do
+    get "signin" => 'devise/session#new'
+    delete "singout" => 'devise/session#destroy'
+    get 'signup' => 'devise/registration#new'
+  end
+
+
   root 'pages#home'
   get 'about' => 'pages#about'
   get 'contactus' => 'pages#contactus'
-  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
